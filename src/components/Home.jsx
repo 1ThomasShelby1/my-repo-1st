b7c1e52d8416f54a98c79c0657f2fb6a4792c9d5
Rutik Bhamare <115616775+1ThomasShelby1@users.noreply.github.com>
2025-05-15 14:25:49 +0530

    Your commit message

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,7 +2,7 @@ import threePhones from '/3_imges.png';
 
 const Compo1 = () => {
   return (
-    <div id="Home" className="relative bg-[linear-gradient(180deg,#FFFFFF_23.36%,#FD746C_100%)] h-screen  md:h-screen lg:h-screen xl:h-screen sm:h-screen xs:h-[450px] max-w-screen">
+    <div id="Home" className="relative bg-[linear-gradient(180deg,#FFFFFF_23.36%,#FD746C_100%)]   md:h-screen lg:h-screen xl:h-screen sm:h-screen h-[450px] max-w-screen">
 
       {/* Text Section */}
       <div className="text-center px-4 py-4 pt-28">
